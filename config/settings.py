8b6462f729e459cf4440fee5af5587d3b3ca91fd
roman <roman.ecm@mail.ru>
2023-08-28 11:20:23 +0500

    refactor settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--r49foo4fr2_jszdj@%^sh4dvv2hnl1oht$o0r2-hg6o94tcq2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
