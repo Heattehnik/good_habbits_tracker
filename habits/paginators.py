62065768914433a5870b6a92aa372f3259f47329
roman <roman.ecm@mail.ru>
2023-08-28 11:41:10 +0500

    add place paginator

--- habits/paginators.py
+++ habits/paginators.py
@@ -3,4 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class HabitsPaginator(PageNumberPagination):
     page_size = 5
-    
+
+
+class PlacesPaginator(PageNumberPagination):
+    page_size = 5
