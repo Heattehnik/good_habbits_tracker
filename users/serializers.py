92effce9ee5c48285638dcb5b124b44accefe7d0
roman <roman.ecm@mail.ru>
2023-08-28 14:25:58 +0500

    add registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,13 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
+from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
-from users.serializers import MyTokenObtainPairSerializer
+
+from users.models import User
+from users.serializers import MyTokenObtainPairSerializer, UserSerializer
 
 
 class TokenObtain(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class UserCreateView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(serializer.validated_data["password"])
+        user.save()
+        return user
