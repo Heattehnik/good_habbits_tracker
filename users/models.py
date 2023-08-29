3b7c1daedc19cfecdaead34c7e326192fb937710
roman <roman.ecm@mail.ru>
2023-08-29 16:12:37 +0500

    update models.py, add migrations

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     city = models.CharField(max_length=100, default='Москва', verbose_name='город')
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users', verbose_name='Аватар', **NULLABLE)
+    chat_id = models.IntegerField(default=0, verbose_name='id чата')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
